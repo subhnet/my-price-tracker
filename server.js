e3f2ed7d4ab9a0d0cdf69c9e6ed1308c32dadd86
subhmaharana <subhransumaharana@gmail.com>
2019-10-07 01:25:41 +0530

    Adding Email Notification

--- server.js
+++ server.js
@@ -2,24 +2,84 @@ var express = require("express");
 var request = require("request");
 var fs = require("fs");
 var cheerio = require("cheerio");
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+const cron = require('node-cron')
 
 
 var app = express();
 
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD
+    }
+});
 
-app.get("/track", (req, res) => {
+let mailOptions = {
+    from: process.env.MAIL_USERNAME,
+    to: 'subhransumaharana@gmail.com',
+    subject: 'Shubh\'s Product Alert',
+};
 
-    let url = 'https://www.amazon.in/dp/B07GB324YK/'
+
+//Checks every minute..
+cron.schedule("* * * * *", () => {
+    console.log('Checking for product..');
+    // let url = 'https://www.amazon.in/dp/'+ req.params.productId;
+    let url = 'https://www.amazon.in/dp/B07SRW3MCM/'
+    console.log(url);
 
     request(url, (err, res, html) => {
         if (!err) {
             var $ = cheerio.load(html);
-            console.log('Success!!');
-            var test = $('#priceblock_ourprice').text();
-            if (test != null && typeof test != 'undefined') {
-                if (test.trim() != "") {
+            console.log('Page loaded!');
+            var priceDetail = $('#priceblock_ourprice').text();
+            if (priceDetail != null && typeof priceDetail != 'undefined' && priceDetail.trim() != "") {
+                console.log("Product is in stock::")
+                console.log("Price is::", priceDetail);
+                transporter.sendMail({ ...mailOptions, text: "This Product is now available at Price::" + priceDetail }, (err, info) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("Email Sent Successfully:" + info.response);
+                    }
+                });
+            } else {
+                console.log("Product still not available..");
+            }
+
+        } else {
+
+            console.log("Error Occured!!", err);
+        }
+    });
+});
+
+
+
+app.get("/track/:productId", (req, res) => {
+    let url = 'https://www.amazon.in/dp/' + req.params.productId;
+    // let url = 'https://www.amazon.in/dp/B07GB324YK/'
+    console.log(url);
+
+    request(url, (err, res, html) => {
+        if (!err) {
+            var $ = cheerio.load(html);
+            console.log('Page loaded!');
+            var priceDetail = $('#priceblock_ourprice').text();
+            if (priceDetail != null && typeof priceDetail != 'undefined') {
+                if (priceDetail.trim() != "") {
                     console.log("Product is in stock::")
-                    console.log("Price is::", test);
+                    console.log("Price is::", priceDetail);
+                    transporter.sendMail({ ...mailOptions, text: "This Product is now available at Price::" + priceDetail }, (err, info) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log("Email Sent Successfully:" + info.response);
+                        }
+                    });
                 }
             }
 
